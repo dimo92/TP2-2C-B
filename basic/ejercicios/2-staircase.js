063f9813ec7c8d758848c79b60e3eebcde1d7774
dimo92 <marianodimodugno@gmail.com>
2020-08-30 17:14:40 -0300

    Done Excersice 2

---
+++ basic/ejercicios/2-staircase.js
@@ -0,0 +1,29 @@
+var defaultStepChar = "#";
+var defaultSpaceChar = " ";
+
+function defaultStairGenerator(stairSize) {
+    customStairGenerator(stairSize, defaultStepChar);
+}
+
+function customStairGenerator(stairSize, stepChar) {
+    generator(stairSize, stepChar);
+}
+
+function generator(a, b) {
+    for (let i = 0; i < a; i++) {
+        let steps = Array(i);
+        let spaces = Array(a - i);
+
+        steps.fill(b);
+        spaces.fill(defaultSpaceChar);
+        console.log(spaces.join("") + steps.join(""));
+
+    }
+}
+
+
+//Generate stair with default steps #
+defaultStairGenerator(6);
+
+//Generate stair with custom steps
+customStairGenerator(6, "$");
